b0ed31ce0b3a9e27953a8afa3aef21fadd49126a
unknown <Aprendiz@BOGDFPCGMFN4162.FormacionSena.RED>
2024-02-06 17:20:16 -0500

    primeras entidades

---
+++ app/entities.py
@@ -0,0 +1,10 @@
+class Carro():
+    placa = "SDF 456"
+    tipo_vehiculo = "Camion"
+    
+class Cliente():
+    nombre = "Cristian aaaa"
+    celular = 3436783548
+    identificacion = 2745834768
+        
+    
